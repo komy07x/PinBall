ee5feaad82f3a99b57abd805f7e3a7e99d9f1a6a
小針茉優子 <komy8115@ezweb.ne.jp>
2017-03-21 23:41:22 +0900

    PinBallプロジェクト　3Dゲーム
    PinBallの3Dゲームです。

---
+++ Assets/Score.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	int score = 0;
+	private GameObject scoreText;
+
+	// Use this for initialization
+	void Start () {
+
+		scoreText = GameObject.Find ("ScoreText");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.tag == "SmallStarTag") {
+			score += 10;
+		} else if (collision.gameObject.tag == "LargeStarTag") {
+			score += 20;
+		} else if (collision.gameObject.tag == "SmallCloudTag") {
+			score += 30;
+		} else if (collision.gameObject.tag == "LargeCloudTag") {
+			score += 50;
+		}
+		scoreText.GetComponent<Text> ().text = "score " + score.ToString();
+	}
+}
